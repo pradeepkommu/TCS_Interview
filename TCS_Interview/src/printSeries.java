bdffe8cbfd994703f503e9101cf086ce713c4fdb
pradeepkommu <nd.naani@gmail.com>
2019-07-14 07:35:26 +0400

    fab

--- TCS_Interview/src/printSeries.java
+++ TCS_Interview/src/printSeries.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class printSeries {
 
+	public static void main(String[] args) 
+    {
+        int n, a = 1, b = 1, c = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        n = s.nextInt();
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= n; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+    }
+	
+	
 }
